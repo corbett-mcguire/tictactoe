dba35c5605de43039571ac7f690cffe01af65e75
Corbett McGuire <corbett.mcguire@gmail.com>
2021-11-30 11:26:35 -0500

    commit and push

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const player1 = 'x'
-const player2 = 'o'
+const player1 = "X";
+const player2 = "O";
+
+let gameStart = true;
+let currentPlayer = player1;
+let cellStore = ["", "", "", "", "", "", "", "", ""];
+
 const win_conditions = [
     [0, 1, 2],
     [3, 4, 5],
@@ -9,14 +14,59 @@ const win_conditions = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
-]
+];
+
+function playerClick(clickedCell, clickedCellIndex) {
+    const playerClick = clickedCell.target;
+    const clickedCell = parseInt(clickedCell.getAttribute('data-cell-index'));
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function changePlayerTurn() {
+    currentPlayer = currentPlayer === player1 ? player2 : player1
+}
+function gameResult() {
+    let gameWin = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winConditions[i]
+        let a = gamePhase[winCondition[0]];
+        let b = gamePhase[winCondition[1]];
+        let c = gamePhase[winCondition[2]];
+        if (a === "" || b === "" || c === "") {
+            continue;
+
+        }
+        if (a === b && b === c) {
+            gameWin = true;
+            break
+        
+        }
+
+}
+
+if (gameWin) {
+    statusDisplay.innerHTML = winText();
+    gameStart = false;
+    return;
+}
+
+
 
 const gridElement = document.getElementById("grid");
 const cellElements = document.querySelector("[data-cell]");
 const winTextElement = document.getElementById("winText");
 // player 1 check
 const winTextCheckElement = document.getElementById("winTextElement");
-let player2_turn = false
+
+function gameRestart() {
+    gameRestart = true;
+    grid = ["", "", "", "", "", "", "", "", ""];
+    currentPlayer = player1;
+    document.querySelectorAll(".cell").forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll(".cell").forEach(cell => cell.addEventListener ("click", playerClick));
+document.querySelectorAll("restartButton").addEventListener("click", gameRestart);
 
 
 
